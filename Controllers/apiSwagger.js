2868b51cf302bcfa9a9db99616478044cfee153a
João Pedro Brandão e Silva <38329386+jpbrs@users.noreply.github.com>
2021-06-11 19:34:41 -0300

    Add Swagger

---
+++ Controllers/apiSwagger.js
@@ -0,0 +1,13 @@
+const swaggerUi = require('swagger-ui-express');
+swaggerDocument = require('../swagger.json');
+
+
+module.exports = app => {
+
+    app.use(
+        '/api-docs',
+        swaggerUi.serve, 
+        swaggerUi.setup(swaggerDocument)
+      );
+
+}
